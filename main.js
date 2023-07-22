619e6a4999df58480ef3f0857ae2e14c61d6d3ae
Shrikrushna Ade <shrikrushnaade766@gmail.com>
2023-07-22 13:03:48 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('.fa-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click',function(){
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+});
